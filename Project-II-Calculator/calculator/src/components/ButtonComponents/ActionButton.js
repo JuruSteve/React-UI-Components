b27922daa7f621e89e8a6561a660f207c912b756
jurusteve <jstevemu17@gmail.com>
2019-03-19 19:33:27 -0400

    completed mvp

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+const ActionButton = props => {
+  console.log(props);
+  return (
+    <div className="action-button1" id={props.clearButton}>
+      <button className={props.buttonStyle}>{props.text}</button>
+    </div>
+  );
+};
+
+export default ActionButton;
